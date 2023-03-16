2202082101c64ec29c7badbb2c454e0a1209f3ed
mateusvg <mateusvitorino@outlook.com.br>
2023-03-16 07:21:30 -0300

    server adjustments

--- bin/server.js
+++ bin/server.js
@@ -1,20 +1,8 @@
 const app = require('../src/app');
 const conn = require('../src/db/connection');
 
-const PORT = process.env.PORT || 3000;
 
-function normalizaPort(val) {
-    const port = parseInt(val, 10);
-    if (isNaN(port)) {
-        return val;
-    }
 
-    if (port >= 0) {
-        return port;
-    }
-
-    return false;
-}
 conn.connect((err) => {
     if (err) {
         console.log('Erro connecting to database...', err)
@@ -23,6 +11,8 @@ conn.connect((err) => {
     console.log('Connection established!')
 })
 
-app.listen(port, function () {
-    console.log(`app listening on port ${port}`)
-})
+
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
