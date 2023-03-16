d11588f0cbedcdb191e952ab0e30793bc13ae8f2
mateusvg <mateusvitorino@outlook.com.br>
2023-03-16 07:26:03 -0300

    port heroku

--- bin/server.js
+++ bin/server.js
@@ -1,8 +1,20 @@
 const app = require('../src/app');
 const conn = require('../src/db/connection');
 
+const PORT = process.env.PORT || 3000;
 
+function normalizaPort(val) {
+    const PORT = parseInt(val, 10);
+    if (isNaN(PORT)) {
+        return val;
+    }
 
+    if (PORT >= 0) {
+        return PORT;
+    }
+
+    return false;
+}
 conn.connect((err) => {
     if (err) {
         console.log('Erro connecting to database...', err)
@@ -11,8 +23,7 @@ conn.connect((err) => {
     console.log('Connection established!')
 })
 
+app.listen(PORT, function () {
+    console.log(`app listening on PORT ${PORT}`)
+})
 
-
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
