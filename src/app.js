7184dad82a12f7fa9decfca3145723efae53989e
mateusvg <mateusvitorino@outlook.com.br>
2023-03-16 08:30:49 -0300

    use cors delete trash code

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 // var corsOptions = {
 // 	origin: "http://localhost:3000"
 // };
-app.use(cors());app.use((req, res, next) => {
+
+app.use((req, res, next) => {
     //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
     res.header("Access-Control-Allow-Origin", "*");
     //Quais são os métodos que a conexão pode realizar na API
