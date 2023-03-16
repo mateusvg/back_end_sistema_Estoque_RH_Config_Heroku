4e4e7ca277273e91dc47fb91591e8365e6d52ec6
mateusvg <mateusvitorino@outlook.com.br>
2023-03-16 06:45:05 -0300

    package json update

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,7 @@
 const app = require('../src/app');
 const conn = require('../src/db/connection');
 
-const port = normalizaPort(process.env.PORT || '8080');
+const PORT = process.env.PORT || 3000;
 
 function normalizaPort(val) {
     const port = parseInt(val, 10);
